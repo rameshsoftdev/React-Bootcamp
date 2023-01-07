c8d1fe9231b940e57cde6b993b9ac311c0864763
rameshsoftdev <112886843+rameshsoftdev@users.noreply.github.com>
2023-01-07 23:40:14 +0530

    Created a project Food Villa

--- App.js
+++ App.js
@@ -1,33 +1,85 @@
 import React from "react";
 import ReactDOM from "react-dom/client";
+import restaurentlist from "./data";
+const cloudinaryUrl = 'https://res.cloudinary.com/swiggy/image/upload/fl_lossy,f_auto,q_auto,w_508,h_320,c_fill/';
+const Header = () => {
+  return (
+    <div className="header">
+      <img
+        className="logo"
+        src="https://content.jdmagicbox.com/comp/jabalpur/x1/9999px761.x761.170314031501.t9x1/catalogue/efoodadda-cherital-jabalpur-cake-delivery-services-801nr3b.jpg"
+      />
+      <NavBar />
+    </div>
+  );
+};
 
-const Relement = <h1>Heading 2</h1>;
+const NavBar = () => {
+  return (
+    <div className="nav-items">
+      <ul>
+        <li>Home</li>
+        <li>About Us</li>
+        <li>Contact</li>
+        <li>Cart</li>
+      </ul>
+    </div>
+  );
+};
 
-// 1st Way of Functional component
-const Rcomponet = ()=>{
-  return <div>
-  <h2>This is from Rcomponet</h2>
-</div>
+const RestauentCard = ({
+  name,
+  area,
+  cloudinaryImageId,
+  cuisines,
+  avgRating,
+}) => {
+  console.log(name, area, cloudinaryImageId, cuisines, avgRating);
+  return (
+    <div className="card">
+      <img
+        className="card-image"
+        src={cloudinaryUrl+cloudinaryImageId}
+      />
+      <h4 className="card-name">{name}</h4>
+      <h4 className="cuisines">{cuisines.join(", ")}</h4>
+      <h4 className="rating">{avgRating}</h4>
+    </div>
+  );
+};
+
+const Body = () => {
+  return (
+    <div className="body-section">
+      <div className="search-area"></div>
+      <div className="restaurentlist">
+        {restaurentlist.map((restaurent) => {
+          return <RestauentCard {...restaurent.data} key={restaurent.data.id}/>;
+        })}
+      </div>
+    </div>
+  );
+};
+
+const Footer = () => {
+  return <div>Footer</div>;
 };
 
 // 2nd Way of Functional component
-const RcomponetAnotherWay = ()=>(
+const RcomponetAnotherWay = () => (
   <div>
     <h2>This is from RcomponetAnotherWay</h2>
   </div>
 );
 
-
-const AppLayout = ()=>{
+const AppLayout = () => {
   return (
     <div>
-      {Relement}
-      <Rcomponet />
-      <RcomponetAnotherWay />
-      {Rcomponet()}
-      <h2>App Component</h2>  
+      <Header />
+      <Body />
+      <Footer />
     </div>
-  )
-}
-const root = ReactDOM.createRoot(document.getElementById('root'));
-root.render(<AppLayout/>);
+  );
+};
+const root = ReactDOM.createRoot(document.getElementById("root"));
+root.render(<AppLayout />);
